835cd0dddb1b62d8c57b82b5078dca2d8da284d5
laon <laon198@gmail.com>
2021-11-06 16:03:11 +0900

    create/ProfessorService

---
+++ src/main/java/application/ProfessorService.java
@@ -0,0 +1,22 @@
+package application;
+
+import domain.model.Lecture;
+import domain.model.LectureID;
+import domain.model.ProfessorID;
+import domain.repository.LectureRepository;
+
+public class ProfessorService {
+    LectureRepository lectureRepo;
+
+    public ProfessorService(LectureRepository lectureRepo){
+        this.lectureRepo = lectureRepo;
+    }
+
+    public void writeLecturePlanner(long lectureID, String itemName,
+                                    String content, long writer){
+        Lecture targetLecture = lectureRepo.findByID(lectureID);
+        targetLecture.writePlanner(itemName, content, writer);
+    }
+
+    //TODO : 강의계획서 업데이트 기능?
+}
