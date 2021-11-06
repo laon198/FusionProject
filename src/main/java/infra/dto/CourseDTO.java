0249229bac1cd3f68b3ea366a705db5362290222
laon <laon198@gmail.com>
2021-11-06 16:04:42 +0900

    create/CourseDTO

---
+++ src/main/java/infra/dto/CourseDTO.java
@@ -0,0 +1,10 @@
+package infra.dto;
+
+public class CourseDTO {
+    private long id;
+    private String courseCode;
+    private String department;
+    private String courseName;
+    private int targetYear;
+    private int credit;
+}
