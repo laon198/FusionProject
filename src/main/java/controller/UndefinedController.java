2dfa42b717db1855372303bd414cbbd078852db5
eubin001122 <lwb001122@naver.com>
2021-11-28 11:46:33 +0900

    login

--- src/main/java/controller/UndefinedController.java
+++ src/main/java/controller/UndefinedController.java
@@ -1,6 +1,7 @@
 package controller;
 
 import application.AccountAppService;
+import com.google.protobuf.DescriptorProtos;
 import domain.repository.AccountRepository;
 import infra.dto.AccountDTO;
 import infra.network.*;
@@ -17,80 +18,56 @@ public class UndefinedController {
     public static final int ADMIN_TYPE = 3;
 
     private AccountRepository accRepo;
-    private Socket socket;
-    private InputStream is;
-    private OutputStream os;
-    private int clientID;
+    private ProtocolService ps;
 
-    public UndefinedController(Socket socket, InputStream is, OutputStream os,
-                               int clientID, AccountRepository accRepo){
-        this.socket = socket;
-        this.is = is;
-        this.os = os;
-        this.clientID = clientID;
+    public UndefinedController(OutputStream os, AccountRepository accRepo){
         this.accRepo = accRepo;
+        ps = new ProtocolService(os);
     }
 
-    public int handler(Protocol recvPt){
+    public int handler(Protocol recvPt) {
         try{
             switch (recvPt.getCode()) {
                 case Protocol.T1_CODE_LOGIN:   // 로그인
                     return loginReq(recvPt);
-                case Protocol.T1_CODE_CREATE: //admin 생성
+                case Protocol.T1_CODE_CREATE:  // 관리자 계정 생성
                     createAdmin(recvPt);
-                    return USER_UNDEFINED;
-                case Protocol.T1_CODE_LOGOUT:  // 로그아웃
-                    logoutReq();
-                    break;
+                    return USER_UNDEFINED; //TODO 관리자 계정 생성하면 관리자 TYPE return?
+                default:                       // 비정상 접근
+                    ps.responseFail();
             }
-        } catch (IOException e) {
-            e.printStackTrace();
         } catch (Exception e) {
             e.printStackTrace();
         }
         return USER_UNDEFINED;
     }
 
-    private int loginReq(Protocol recvPt) throws Exception{
-        Protocol sendPt = new Protocol(Protocol.TYPE_RESPONSE);
-        AccountDTO accDTO = (AccountDTO) recvPt.getObject();
-
-        try{
+    private int loginReq(Protocol recvPt) throws IOException {
+        AccountDTO resAccDTO = null;
+        try {
+            AccountDTO accDTO = (AccountDTO) recvPt.getObject();
             AccountAppService accService = new AccountAppService(accRepo);
-            AccountDTO resAccDTO= accService.login(accDTO);
-
-            // < DB >
-            // 로그인 성공
-            sendPt.setCode(Protocol.T2_CODE_SUCCESS);
-            sendPt.setObject(resAccDTO);
-            sendPt.send(os);
-
-            if(resAccDTO.getPosition().equals("STUD")){
-                return STUD_TYPE;
-            }else if(resAccDTO.getPosition().equals("PROF")){
-                return PROF_TYPE;
-            }else{
-                return ADMIN_TYPE;
-            }
-        }catch(IllegalArgumentException e){
-//            // 로그인 실패
-            sendPt.setCode(Protocol.T2_CODE_FAIL);
-            sendPt.send(os);
+            resAccDTO= accService.login(accDTO);
+        } catch (Exception e) {   // 로그인 실패 예외 받음
+            ps.responseFail();    // 로그인 실패 메시지 전송
+            return USER_UNDEFINED;
         }
 
-        return USER_UNDEFINED;
+        ps.reponseSuccess();
+        if(resAccDTO.getPosition().equals("STUD")){
+            return STUD_TYPE;
+        }else if(resAccDTO.getPosition().equals("PROF")){
+            return PROF_TYPE;
+        }else {
+            return ADMIN_TYPE;
+        }
     }
+
+
 
     private void createAdmin(Protocol recvPt){
 //        if(recvPt.getEntity()!=Protocol.ENTITY_AD)
     }
-
-    private void logoutReq() throws IOException {
-        Protocol sendPt = new Protocol(Protocol.TYPE_RESPONSE);
-        sendPt.setCode(Protocol.T2_CODE_SUCCESS);
-        sendPt.send(os);
-    }
-
 
 }
 
